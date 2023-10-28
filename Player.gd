9111900e73420c6e06a2f1e4220178daf29f4d51
Joseph Sharratt <joseph.sharratt@gmail.com>
2023-10-28 15:10:18 +0100

    Move Player with physics instead of position change

--- Player.gd
+++ Player.gd
@@ -1,13 +1,25 @@
-extends Area2D
+extends CharacterBody2D
 
 var move_speed_px : int = 50
 
-func _process(delta):
+func _physics_process (delta):
+	velocity = Vector2()
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	position += direction * move_speed_px * delta
+	
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	
+	velocity = velocity.normalized() * move_speed_px
+	move_and_slide()
 	handle_animation(direction)
 
-func handle_animation(direction):
+func handle_animation (direction):
 	if direction.x > 0:
 		$AnimatedSprite2D.play("right")
 	elif direction.x < 0:
